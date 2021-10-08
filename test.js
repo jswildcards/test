da2e70c33b7d66b5c99e546dceb0cbdcbd183720
jswildcards <eggkuv35c@relay.firefox.com>
2021-10-08 12:31:44 +0800

    feat: calculate sum

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 // simple output
-console.log("hi")
+console.log("hi");
 
 // fetch data
-fetch("https://www.example.com").then(res => res.json())
+fetch("https://www.example.com").then((res) => res.json());
+
+// calculate sum from a number array using reduce
+const array = [1, 2, 3, 4, 5];
+console.log(array.reduce((prev, cur) => prev + cur, 0)); // 15
